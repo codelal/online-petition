76e62cf6ef1ce17dca9b770318e0f72af926eaef
Alma <al.klein@posteo.de>
2020-12-04 20:37:13 +0100

    jp2

--- jasmine-petition/public/script.js
+++ jasmine-petition/public/script.js
@@ -1,3 +1,36 @@
-(function(){
- const = $("")
-})
+(function () {
+    const canvas = $("canvas");
+    // console.log("canvas", canvas);
+    let ctx = canvas[0].getContext("2d");
+    let canvasInput = $(".canvasInput");
+    let dataUrl;
+
+    let clickStart;
+
+    canvas.on("mousedown", function () {
+        clickStart = true;
+    });
+
+    canvas.on("mousemove", function (event) {
+        if (!clickStart) {
+            return;
+        } else {
+            // ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.lineWidth = 1;
+            ctx.lineCap = "round";
+            ctx.lineTo(event.pageX - 20, event.pageY - 155);
+            ctx.stroke();
+        }
+    });
+
+    canvas.on("mouseup", function () {
+        //console.log("mouseup");
+        clickStart = false;
+        dataUrl = canvas[0].toDataURL();
+        //console.log(dataUrl);
+        //canvasInput.val(`"${dataUrl}"`);
+        canvasInput.val(dataUrl);
+        //console.log(canvasInput.val());
+    });
+})();
