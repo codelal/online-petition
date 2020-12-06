e2af00bbe4f1e1983fc008e7e88d5c1c8178eb36
Alma <al.klein@posteo.de>
2020-12-06 21:26:26 +0100

    PetitionI+II_final

--- jasmine-petition/public/script.js
+++ jasmine-petition/public/script.js
@@ -4,30 +4,38 @@
     let ctx = canvas[0].getContext("2d");
     let canvasInput = $(".canvasInput");
     let dataUrl;
-
     let clickStart;
 
-    canvas.on("mousedown", function () {
+    function draw(event) {
+        //ctx.beginPath();
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 1;
+        ctx.lineCap = "round";
+        ctx.lineTo(event.pageX - 200, event.pageY - 670);
+        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.moveTo(event.pageX - 330, event.pageY -600);
+    }
+
+    canvas.on("mousedown", function (event) {
         clickStart = true;
+        draw(event);
     });
 
     canvas.on("mousemove", function (event) {
+
         if (!clickStart) {
             return;
         } else {
-            // ctx.beginPath();
-            ctx.strokeStyle = "blue";
-            ctx.lineWidth = 1;
-            ctx.lineCap = "round";
-            ctx.lineTo(event.pageX - 20, event.pageY - 155);
-            ctx.stroke();
+            draw(event);
         }
     });
 
     canvas.on("mouseup", function () {
         clickStart = false;
+        ctx.beginPath();
         dataUrl = canvas[0].toDataURL();
         canvasInput.val(dataUrl);
-        console.log(canvasInput.val());
+        //console.log(canvasInput.val());
     });
 })();
