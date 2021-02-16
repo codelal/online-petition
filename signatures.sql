b7592947ed6417b37005b20c6050946320436ac5
Alma <al.klein@posteo.de>
2021-02-16 16:20:21 +0100

    some cleaning

--- signatures.sql
+++ signatures.sql
@@ -7,11 +7,3 @@ DROP TABLE IF EXISTS signatures;
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
  );
 
-
--- here we are adding the foreign key (user_id)
- -- foreign key lets us know which user from the users table signed the petition
- -- and which signature is theirs (links the 2 tables!)
-
---DELETE FROM signatures;
-
- --TEST-- INSERT INTO signatures(first, last, signature) VALUES ('Leonardo', 'DiCaprio', 'signa');
